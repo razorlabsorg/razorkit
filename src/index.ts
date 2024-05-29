3519375db46df2d8fb0eafb3826ace5953e5510c
Nate <89227631+0xNeit@users.noreply.github.com>
2024-05-29 10:37:37 +0100

    Export Wallets and bump version

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ export * from './hooks';
 
 export type { IWalletAdapter as IAptosWalletAdapter } from '@razorlabs/m1-wallet-sdk';
 export type { IWalletAdapter as ISuiWalletAdapter } from '@razorlabs/m2-wallet-sdk';
+export type { IWallet as IAptosWallet } from '@razorlabs/m1-wallet-sdk';
+export type { IWallet as ISuiWallet } from '@razorlabs/m2-wallet-sdk';
 export { AptosChainId } from '@razorlabs/m1-wallet-sdk';
