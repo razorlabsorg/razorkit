89c42cbc9d4c32f5e502802ac1747fbcb3feea23
Nate <89227631+0xNeit@users.noreply.github.com>
2024-08-31 01:51:54 +0100

    Added mevm chain to razorkit export

--- apps/evm-with-vite/src/main.tsx
+++ apps/evm-with-vite/src/main.tsx
@@ -1,10 +1,28 @@
+import './global.css';
+import '@razorlabs/razorkit/styles.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
+import { getDefaultConfig, mevmImola, RazorKitEthProvider } from '@razorlabs/razorkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+
+const config = getDefaultConfig({
+  appName: 'RazorKit Vite MEVM demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mevmImola],
+});
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RazorKitEthProvider>
+          <App />
+        </RazorKitEthProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 )
--- packages/razor-kit/src/index.ts
+++ packages/razor-kit/src/index.ts
@@ -36,3 +36,4 @@ export { midnightTheme } from './themes/midnightTheme';
 export { cssStringFromTheme } from './css/cssStringFromTheme';
 export { cssObjectFromTheme } from './css/cssObjectFromTheme';
 export { __private__ } from './__private__';
+export { mevmImola } from './chains/mevm';
