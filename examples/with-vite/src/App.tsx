260df21246d537d2a942edf3b498e85644c109da
Nate <89227631+0xNeit@users.noreply.github.com>
2025-01-08 12:41:24 +0100

    Enhance Vite example with Razor Kit integration and update dependencies
    - Added '@razorlabs/razorkit' as a dependency in the Vite example project.
    - Updated index.html to use a favicon and preload custom fonts for improved performance.
    - Refactored App.tsx to integrate Razor Kit components, including ConnectButton and wallet management features.
    - Updated main.tsx to wrap the App component with WalletProvider for context.
    - Changed Vite server port from 6000 to 5000 for consistency.
    
    These changes improve the example's functionality and user experience by integrating Razor Kit features and optimizing asset loading.

--- examples/with-vite/src/App.tsx
+++ examples/with-vite/src/App.tsx
@@ -1,35 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import razorLogo from './assets/logo.png';
+import "./App.css";
+import {
+  ConnectButton,
+  useAccountBalance,
+  useWallet,
+  ErrorCode,
+  formatCurrency,
+} from "@razorlabs/razorkit";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const wallet = useWallet();
+  const { balance } = useAccountBalance();
+
+  function uint8arrayToHex(value: Uint8Array | undefined) {
+    if (!value) return "";
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return value.toString("hex");
+  }
+
+  async function handleSignMsg() {
+    if (!wallet.account) return;
+    try {
+      const msg = "Hello world!";
+      const result = await wallet.signMessage({
+        message: msg,
+        nonce: '0',
+      });
+      console.log("verify signedMessage", result);
+      if (!result) {
+        alert(`signMessage succeed, but verify signedMessage failed`);
+      } else {
+        alert(`signMessage succeed, and verify signedMessage succeed!`);
+      }
+    } catch (e) {
+      console.error("signMessage failed", e);
+      alert("signMessage failed (see response in the console)");
+    }
+  }
+
+  const getWalletStatus = () => {
+    if (wallet.connecting) return "connecting";
+    return wallet.connected ? "connected" : "disconnected";
+  };
 
   return (
-    <>
+    <div className="App">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/razorlabsorg/razorkit" target="_blank">
+          <img src={razorLogo} className="logo" alt="Razor logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Razor Kit</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <ConnectButton
+        style={{ marginTop: '16px', background: 'linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)' }}
+        onConnectSuccess={(name) => {
+          console.log('connect success: ', name);
+        }}
+        onConnectError={(err) => {
+          //@ts-ignore
+          if (err.code === ErrorCode.WALLET__CONNECT_ERROR__USER_REJECTED) {
+            console.warn(
+              'user rejected the connection to ' + err.details?.wallet,
+            );
+          } else {
+            console.warn('unknown connect error: ', err);
+          }
+        }}
+        onDisconnectSuccess={(name) => {
+          console.log('disconnect success: ', name);
+        }}
+        onDisconnectError={(err) => {
+          console.log('disconnect error: ', err);
+        }}
+      />
+
+        {!wallet.connected ? (
+          <p>Connect DApp with Razor wallet from now!</p>
+        ) : (
+          <div>
+            <div>
+              <p>current wallet: {wallet.adapter?.name}</p>
+              <p>
+                wallet status: {getWalletStatus()}
+              </p>
+              <p>wallet address: {wallet.account?.address}</p>
+              <p>current network: {wallet.chain?.name}</p>
+              <p>
+                wallet balance:{" "}
+                {formatCurrency(balance ?? 0, {
+                  withAbbr: false,
+                  decimals: 8,
+                })}{" "}
+                MOVE
+              </p>
+              <p>
+                wallet publicKey: {uint8arrayToHex(wallet.account?.publicKey)}
+              </p>
+            </div>
+            <div className={"btn-group"} style={{ margin: "8px 0" }}>
+              <button onClick={handleSignMsg}>signMessage</button>
+            </div>
+          </div>
+        )}
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and Razor logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- examples/with-vite/src/main.tsx
+++ examples/with-vite/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import { WalletProvider } from '@razorlabs/razorkit'
+import "@razorlabs/razorkit/style.css";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WalletProvider>
+      <App />
+    </WalletProvider>
   </StrictMode>,
 )
