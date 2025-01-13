f2c4992b214dc95d194eb8190208d06311735226
karancoder <jivanikaran@gmail.com>
2025-01-13 19:43:45 +0530

    revert formatting changes

--- packages/sdk/src/wallet/AllDefaultWallets.ts
+++ packages/sdk/src/wallet/AllDefaultWallets.ts
@@ -2,7 +2,10 @@ import * as presets from './preset-wallets';
 
 export const AllDefaultWallets = [
   presets.RazorWallet,
-  ...[presets.NightlyWallet, presets.BitgetWallet, presets.OkxWallet, presets.LeapWallet].sort((a, b) =>
-    a.name < b.name ? -1 : 1,
-  ),
+  ...[
+    presets.NightlyWallet,
+    presets.BitgetWallet,
+    presets.OkxWallet,
+    presets.LeapWallet
+  ].sort((a, b) => (a.name < b.name ? -1 : 1)),
 ];
